ff12dc2ed1a50ee83e433c9e9c62d21f09c6a1ab
21223232326 <sevaletyagin@gmail.com>
2024-11-25 18:02:55 +0200

    ttt

---
+++ a.js
@@ -0,0 +1,39 @@
+const siteContent = {
+    eng: {
+        menuHome: "Home",
+        menuCategories: "Categories",
+        menucollections:"collections",
+        menuBlog: "Blog",
+        menulogin: "login",
+        menuSingUp: "SingUp",
+        sec1Str1: "Awesome tools for",
+        sec1Str2: "Designer & Developer",
+        sec1Str3: "Antool is a web collection of information on paid or",
+        sec1Str4: "free Design and Development tools",
+        Search: "Search",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+        sec1Str4: "",
+
+    },
+    ukr: {
+        menuHome: "Головна",
+        menuCategories: "Категорії"
+    }
+    
+}
